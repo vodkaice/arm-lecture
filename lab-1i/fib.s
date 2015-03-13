567eeeb480a67269dacef8aeaf9ee80645b2d816
vodkaice <brandedwolf@gmal.com>
2015-03-14 00:34:11 +0800

    otimize iterative version and add time function
    reduce the register counts,and add the time function in .C to evaluate the performance

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -10,21 +10,19 @@
 
 fibonacci:
 	@ ADD/MODIFY CODE BELOW
-	push {r3,r4,r5,r6,lr}
+	push {r3,r4,r6,lr}
 	mov r3,#-1	@r3=previous
 	mov r4,#1	@r4=result
-	mov r5,#0	@r5=i
 	mov r6,#0	@r6=sum
 loop:
 	add r6,r3,r4	@sum = result + previous
 	mov r3,r4	@previous = result
 	mov r4,r6	@result = sum	
-	add r5,#1	@i++
-	cmp r0,r5	@r0-r5 (x-i)
-	bge loop	@if x>=i jump to loop
+	subs r0,#1	@r0=r0-1
+	bge loop	@if x!=0  jump to loop
 
 	mov r0,r6	@return result
-	pop {r3,r4,r5,r6,pc}
+	pop {r3,r4,r6,pc}
 	@ END CODE MODIFICATION
 
 	.size fibonacci, .-fibonacci
--- lab-1i/fibseq.c
+++ lab-1i/fibseq.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <time.h>
 
 extern int fibonacci(int x);
 
@@ -6,9 +7,14 @@ int main(int argc, char **argv)
 {
   int number=0;
   int result=0;
+  clock_t time;
+  float total_t=0;
 
   scanf("%d",&number);
-  result = fibonacci(number);   
+  time = clock();
+  result = fibonacci(number);
+  total_t =(float)(clock() - time )/CLOCKS_PER_SEC; 
   printf("The fibonacci sequence at %d is: %d\n", number, result);
+  printf("The execute time is : %f second\n",total_t);
 }
 
